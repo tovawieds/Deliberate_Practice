245a82944ff28e4d8470c9acbc734f269846484b
tovawieds <wiedermanntova@gmail.com>
2025-08-14 13:08:51 -0400

    js variables1, 1-5

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+var num = 5;
+console.log(num + 10);
+
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+var word = "hello";
+console.log(word.toUpperCase());
+
+// Write a program that uses variables to store two numbers, then prints the numbers added together.
+var num1 = 5;
+var num2 = 1;
+console.log(num1 + num2);
+
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+var word = "hello";
+console.log(word.split("").reverse().join(""));
+
+// Write a program that uses a variable to store a number, then prints the number times 10.
+var num = 5;
+console.log(num * 10);

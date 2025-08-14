af12865c0232a759a22d86145eb2ffe18f03b982
tovawieds <wiedermanntova@gmail.com>
2025-08-14 17:20:54 -0400

    js variables1, 6-10:

--- javascript.js
+++ javascript.js
@@ -18,3 +18,25 @@ console.log(word.split("").reverse().join(""));
 // Write a program that uses a variable to store a number, then prints the number times 10.
 var num = 5;
 console.log(num * 10);
+
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+var word1 = "hello";
+var word2 = "tova";
+console.log(word1.toUpperCase() + " " + word2.toUpperCase());
+
+// Write a program that uses a variable to store a word, then prints the number of letters in the word.
+var word = "hello";
+console.log(word.length);
+
+// Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+var dec = 45.6;
+console.log(parseInt(dec));
+
+// Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+var num1 = 2;
+var num2 = 6;
+console.log(num1 * num2);
+
+// Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+var word = "heLLO";
+console.log(word.toLowerCase());

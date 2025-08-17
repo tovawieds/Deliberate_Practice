189ae4af457991c55983830f29072dd943ad6bf0
tovawieds <wiedermanntova@gmail.com>
2025-08-17 19:13:21 -0400

    arrays-hashes2, 1-5

--- arrays-hashes.js
+++ arrays-hashes.js
@@ -26,3 +26,39 @@ console.log(arr);
 var arr = ["one", "two", "three"];
 arr[2] = arr[2].toUpperCase();
 console.log(arr);
+
+// ARRAYS-HASHES2
+// Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+var hash = { firstName: "Tova", lastName: "Wieds", email: "t@gmail" };
+console.log(hash.firstName);
+console.log(hash["lastName"]);
+console.log(hash.email);
+
+// Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+var arr = [
+  { firstName: "Tova", lastName: "Wieds" },
+  { firstName: "Ahuva", lastName: "David" },
+  { firstName: "Devo", lastName: "Berko" },
+];
+console.log(arr[0].firstName);
+console.log(arr[0]["lastName"]);
+
+// Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+var menu = { hotDog: 3.99, soda: 2.99, burger: 4.99 };
+menu["fries"] = 6.25;
+console.log(menu);
+
+// Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+var book = { title: "Book", author: "Dena", pages: 304, language: "English" };
+console.log(book.title);
+console.log(book["author"]);
+console.log(book.pages);
+console.log(book.language);
+
+// Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+var books = [
+  { title: "title1", author: "author1" },
+  { title: "title2", author: "author2" },
+  { title: "title3", author: "author3" },
+];
+console.log(books[2].author);

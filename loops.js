f0d464d39e574939bc7cb336da4682889c82d118
tovawieds <wiedermanntova@gmail.com>
2025-08-17 13:16:34 -0400

    js loops2, 1-5

--- loops.js
+++ loops.js
@@ -34,3 +34,67 @@ while (num < 10) {
   console.log(9000);
   num++;
 }
+
+// LOOPS2
+// Start with an array of numbers and create a new array with each number times 3.
+// For example, [1, 2, 3] becomes [3, 6, 9].
+var arr = [1, 2, 3];
+var arr2 = [];
+// for (x = 0; x < arr.length; x++) {
+//   arr2.push(arr[x] * 3);
+// }
+arr.forEach(function (num) {
+  arr2.push(num * 3);
+});
+console.log(arr2);
+
+// Start with an array of strings and create a new array with each string upcased.
+// For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+var arr = ["hello", "goodbye"];
+var arr2 = [];
+arr.forEach(function (word) {
+  arr2.push(word.toUpperCase());
+});
+console.log(arr2);
+
+// Start with an array of hashes and create a new array of string values from each hash's :name key.
+// For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+var arr = [
+  { name: "Tova", age: 21 },
+  { name: "Ahuva", age: 9 },
+];
+var arr2 = [];
+arr.forEach(function (key) {
+  arr2.push(key.name);
+});
+console.log(arr2);
+
+// Start with an array of numbers and create a new array with each number plus 7.
+// For example, [1, 2, 3] becomes [8, 9, 10].
+var arr = [1, 2, 3];
+var arr2 = [];
+arr.forEach(function (num) {
+  arr2.push(num + 7);
+});
+console.log(arr2);
+
+// Start with an array of strings and create a new array with each string's length.
+// For example, ["hello", "goodbye"] becomes [5, 7].
+var arr = ["hello", "goodbye"];
+var arr2 = [];
+arr.forEach(function (word) {
+  arr2.push(word.length);
+});
+console.log(arr2);
+
+count = 0;
+while (count < arr.length) {
+  arr2.push(arr[count].length);
+  count++;
+}
+console.log(arr2);
+
+var arr2 = arr.map(function (word) {
+  return word.length;
+});
+console.log(arr2);

2b38f617afc4c169fe678174221c12eb8320943f
tovawieds <wiedermanntova@gmail.com>
2025-08-17 11:45:28 -0400

    js loops1, 1-5

---
+++ loops.js
@@ -0,0 +1,36 @@
+// Write a while loop to print the numbers 1 through 10.
+var x = 1;
+while (x < 11) {
+  console.log(x);
+  x++;
+}
+
+x++;
+// Write a while loop that prints the word "hello" 5 times.
+var x = 1;
+while (x <= 5) {
+  console.log("hello");
+  x++;
+}
+
+// // Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+// while (true) {
+//   var input = window.prompt("Enter a word: ");
+//   if (input === "stop") {
+//     break;
+//   }
+// }
+
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+var num = 0;
+while (num <= 100) {
+  console.log(num);
+  num += 5;
+}
+
+// Write a while loop that prints the number 9000 ten times.
+var num = 0;
+while (num < 10) {
+  console.log(9000);
+  num++;
+}
